77f3ce54d9fa96934783f2017ff7be50af3f5864
MarkoPantic <cockta90@gmail.com>
2019-07-16 01:21:23 +0200

    Person components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm react app</h1>
+        <Person name='Marko' age='25'/>
+        <Person name='Pavle' age='22'>My Hobbies: Racing</Person>
+        <Person name='Jelena' age='55'/>
       </div>
     );
   }
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// class Person {
+
+// }
+
+// export default Person
+
+
+const person = (props) => {
+    return <p>I'm a {props.name} and I am {props.age}</p>
+}
+
+export default person
